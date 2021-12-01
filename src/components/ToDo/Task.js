5034762968a36273c84ae637d52b485091b61914
mistidinzy <mistidinzy@gmail.com>
2021-12-01 00:15:58 -0600

    add bootstrap toast

--- src/components/ToDo/Task.js
+++ src/components/ToDo/Task.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Toast } from 'react-bootstrap';
 
 
 function Task(props){
@@ -16,14 +17,23 @@ function Task(props){
   // }
 
   return(
-    <ul>
-      <div key={task.id}>
-        <li>{task.name}</li>
-        <li>{task.assignee}</li>
-        <li>{task.difficulty}</li>
-        <li>{task.isComplete}</li>
-      </div>
-    </ul>
+    <Toast>
+      <Toast.Header>
+        <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
+        <strong className="me-auto">Bootstrap</strong>
+        <small className="text-muted">just now</small>
+      </Toast.Header>
+      <Toast.Body>See? Just like this.</Toast.Body>
+    </Toast>
+
+    // <ul>
+    //   <div key={task.id}>
+    //     <li>{task.name}</li>
+    //     <li>{task.assignee}</li>
+    //     <li>{task.difficulty}</li>
+    //     <li>{task.isComplete}</li>
+    //   </div>
+    // </ul>
   )
 }
 
