8735f9edd7c000449d4975933191d6aeed97ef7c
Keith Dahlby <keith@deltavcodeschool.com>
2021-11-30 21:14:25 -0600

    Actually, NavLink is better

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import {Nav, Navbar, Container} from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Header(){
   return(
@@ -8,10 +8,10 @@ function Header(){
           <Container>
             <Navbar.Brand href="#home">To&middot;Do</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
-              <Nav.Link as={Link} to="/About">About</Nav.Link>
-              <Nav.Link as={Link} to="/List">To&middot;Do List</Nav.Link>
-              <Nav.Link as={Link} to="/Form">Add New Task</Nav.Link>
+              <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+              <Nav.Link as={NavLink} to="/About">About</Nav.Link>
+              <Nav.Link as={NavLink} to="/List">To&middot;Do List</Nav.Link>
+              <Nav.Link as={NavLink} to="/Form">Add New Task</Nav.Link>
             </Nav>
           </Container>
       </Navbar>
