1efd7752655a7afdbaeb4835eadaffef22ecf060
Keith Dahlby <keith@deltavcodeschool.com>
2021-11-30 21:10:32 -0600

    Use react-router-dom Link with React Bootstrap Nav.Link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
 import {Nav, Navbar, Container} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Header(){
   return(
@@ -7,10 +8,10 @@ function Header(){
           <Container>
             <Navbar.Brand href="#home">To&middot;Do</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/About">About</Nav.Link>
-              <Nav.Link href="/List">To&middot;Do List</Nav.Link>
-              <Nav.Link href="/Form">Add New Task</Nav.Link>
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/About">About</Nav.Link>
+              <Nav.Link as={Link} to="/List">To&middot;Do List</Nav.Link>
+              <Nav.Link as={Link} to="/Form">Add New Task</Nav.Link>
             </Nav>
           </Container>
       </Navbar>
