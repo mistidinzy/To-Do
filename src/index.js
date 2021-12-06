f6b36b1aa28663eed73e41cb9fa25b6b57397792
mistidinzy <mistidinzy@gmail.com>
2021-12-06 09:54:04 -0600

    add authProvider call to index

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './hooks/useAuth';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
+    <AuthProvider>
       <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
